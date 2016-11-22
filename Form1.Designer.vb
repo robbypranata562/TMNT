3d4727c8f3b505d941cb8b1bf283e88baf8c8854
robbypranata562 <robbypranata562@gmail.com>
2016-11-22 17:15:44 +0700

    First Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits MetroFramework.Forms.MetroForm
+
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MetroStyleManager1 = New MetroFramework.Components.MetroStyleManager(Me.components)
+        Me.GC = New Braincase.GanttChart.Chart()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        CType(Me.MetroStyleManager1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MetroStyleManager1
+        '
+        Me.MetroStyleManager1.Owner = Me
+        Me.MetroStyleManager1.Style = MetroFramework.MetroColorStyle.Black
+        Me.MetroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'GC
+        '
+        Me.GC.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.GC.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GC.FullDateStringFormat = Nothing
+        Me.GC.Location = New System.Drawing.Point(20, 60)
+        Me.GC.Margin = New System.Windows.Forms.Padding(0)
+        Me.GC.Name = "GC"
+        Me.GC.Size = New System.Drawing.Size(685, 394)
+        Me.GC.TabIndex = 0
+        '
+        'ToolTip1
+        '
+        Me.ToolTip1.AutomaticDelay = 100
+        Me.ToolTip1.IsBalloon = True
+        Me.ToolTip1.ShowAlways = True
+        Me.ToolTip1.ToolTipTitle = "dada"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(725, 474)
+        Me.Controls.Add(Me.GC)
+        Me.Name = "Form1"
+        Me.Text = "Project"
+        Me.Theme = MetroFramework.MetroThemeStyle.Dark
+        CType(Me.MetroStyleManager1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents MetroStyleManager1 As MetroFramework.Components.MetroStyleManager
+    Friend WithEvents GC As Braincase.GanttChart.Chart
+    Public WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports Braincase.GanttChart
+
+
+Public Class Form1
+    Dim TMNT As New ProjectManager()
+    Dim Tasking As New Task() With {.Name = "Tasking 1"}
+    Dim Tasking2 As New Task() With {.Name = "Tasking 2"}
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Tasking.Name("Tasking 1")
+
+
+        'TMNT.
+
+    End Sub
+
+    Private Sub GC_Load(sender As Object, e As EventArgs) Handles GC.Load
+        TMNT.Add(Tasking)
+        'TMNT.SetDuration(Tasking, 4)
+        TMNT.SetComplete(Tasking, 0.3)
+        TMNT.Add(Tasking2)
+        'TMNT.SetDuration(Tasking2, 2)
+        TMNT.SetComplete(Tasking2, 0.9)
+        TMNT.SetStart(Tasking, 1)
+        TMNT.SetEnd(Tasking, 3)
+        TMNT.SetStart(Tasking2, 4)
+        TMNT.SetEnd(Tasking2, 6)
+
+        GC.Init(TMNT)
+    End Sub
+
+    Private Sub GC_TaskSelected(sender As Object, e As TaskMouseEventArgs) Handles GC.TaskSelected
+        'GC.SetToolTip((Task), Tasking.Name, Tasking.Complete.ToString)
+        'If e.Task Then
+
+        'End If
+        'MessageBox.Show(Tasking.Name + " " + Tasking.Start.ToString + " " + Tasking.End.ToString + " " + Tasking.Duration.ToString + " " + Tasking.Complete.ToString)
+        'ToolTip1.Show("sda", Me)
+        MessageBox.Show(e.Task.ToString
+                        )
+    End Sub
+
+    Private Sub GC_MouseHover(sender As Object, e As EventArgs) Handles GC.MouseHover
+
+        ' MessageBox.Show(Tasking.Name + " " + Tasking.Start.ToString + " " + Tasking.End.ToString + " " + Tasking.Duration.ToString + " " + Tasking.Complete.ToString)
+     
+    End Sub
+End Class
